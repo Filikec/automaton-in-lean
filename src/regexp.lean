e68554cb93e1d6db4f4d0dddf00d930577d3f86d
ChenQuanwen <git@cqw.one>
2022-09-16 17:04:26 +0100

    Refactor proofs
    separate construction of re2nfa and its corresponding lemma

--- src/regexp.lean
+++ src/regexp.lean
@@ -8,7 +8,7 @@ section re
 
 variables {Sigma : Type} [decidable_eq Sigma]
 
-inductive RE (Sigma : Type*)
+inductive RE (Sigma : Type)
 | empty : RE
 | lit : Sigma → RE
 | union : RE → RE → RE
@@ -17,6 +17,15 @@ inductive RE (Sigma : Type*)
 | append : RE → RE → RE
 
 open RE
+
+def empty_lang : lang Sigma
+:= λ _, false
+
+def epsilon_lang : lang Sigma
+:= λ w, w = []
+
+def lit_lang (x : Sigma) : lang Sigma
+:= λ w, w = x :: []
 
 def union_lang (P Q : lang Sigma) : lang Sigma 
 := λ w , P w ∨ Q w 
@@ -30,12 +39,422 @@ def append_lang (P Q : lang Sigma) : lang Sigma
 := λ w, ∃ u v : word Sigma, P u ∧ Q v ∧ w = u ++ v    
 
 def re_lang : RE Sigma → lang Sigma
-| empty := λ w , false
-| epsilon := λ w, w = []
-| (lit x) := λ w, w = x :: []
+| empty := empty_lang
+| epsilon := epsilon_lang
+| (lit x) := lit_lang x 
 | (union r s) := union_lang (re_lang r) (re_lang s)
 | (star r) := star_lang (re_lang r) 
 | (append r s) := append_lang (re_lang r) (re_lang s)
+
+def empty_ε_nfa {Sigma : Type*} [decidable_eq Sigma] : ε_nfa Sigma :=
+  {
+    Q := fin 1,
+    finQ := by apply_instance,
+    decQ := by apply_instance,
+    inits := λ _ , true,
+    decI := by apply_instance,
+    final := λ _ , false,
+    decF := by apply_instance,
+    δ := λ _ _ _ , false,
+    decD := λ _, by {dsimp[sigma.uncurry], apply_instance,},
+  }
+
+lemma empty_ε_nfa_lang : ∀ w : word Sigma, ε_nfa_lang empty_ε_nfa w ↔ empty_lang w :=
+begin 
+  assume w,
+  dsimp [ε_nfa_lang, empty_lang],
+  constructor,
+  {
+    assume h,
+    cases h with q0 h, cases h with q1 h,
+    cases and.elim_right (and.elim_right h),
+  },
+  {
+    assume f,
+    cases f,
+  }
+end
+
+def epsilon_ε_nfa {Sigma : Type*} : ε_nfa Sigma :=
+  {
+    Q := fin 1,
+    finQ := by apply_instance,
+    decQ := by apply_instance,
+    inits := λ _ , true,
+    decI := by apply_instance,
+    final := λ _ , true,
+    decF := by apply_instance,
+    δ := λ _ _ _ , false,
+    decD := λ _, by {dsimp[sigma.uncurry], apply_instance,},
+  }
+
+lemma epsilon_ε_nfa_lang : ∀ w : word Sigma, ε_nfa_lang epsilon_ε_nfa w ↔ epsilon_lang w :=
+begin 
+  assume w,
+  dsimp [ε_nfa_lang, epsilon_lang],
+  constructor,
+  {
+    assume h,
+    cases h with q0 h, cases h with q1 h,
+    cases (and.elim_left (and.elim_right h)),
+    refl,
+    cases ᾰ,
+    cases ᾰ,
+  },
+  {
+    assume h,
+    let z : fin 1,
+      exact 0,
+    existsi z, existsi z,
+    constructor,
+    trivial,
+    constructor,
+    rw h,
+    fconstructor,
+    trivial,
+  }
+end
+
+def single_ε_nfa {Sigma : Type*} [decidable_eq Sigma] (lit : Sigma) : ε_nfa Sigma :=
+  {
+    Q := fin 2,
+    finQ := by apply_instance,
+    decQ := by apply_instance,
+    inits := λ x , x.val = 0,
+    decI := by apply_instance,
+    final := λ x , x.val = 1,
+    decF := by apply_instance,
+    δ := λ q0 x q1 , q0.val = 0 ∧ x = lit ∧ q1.val = 1,
+    decD := begin
+      assume x,
+      dsimp [sigma.uncurry],
+      apply_instance,
+    end
+  }
+
+lemma single_ε_nfa_lang : ∀ x : Sigma, ∀ w : word Sigma, ε_nfa_lang (single_ε_nfa x) w ↔ lit_lang x w :=
+begin
+  assume x w,
+  dsimp [ε_nfa_lang, lit_lang],
+  constructor,
+  {
+    assume h,
+    cases h with q0 h, cases h with q1 h,
+    cases (and.elim_left (and.elim_right h)),
+    {
+      cases h with h1 h, cases h with h2 h3,
+      have z : q0.val = 0,
+        exact h1,
+      have f : false,
+        have o : q0.val = 1,
+          exact h3,
+        finish,
+      cases f, 
+    },
+    {
+      cases ᾰ with a b, cases b with b c,
+      cases b,
+      have t: w_1 = [],
+      {
+        cases ᾰ_1,
+        refl,
+        cases ᾰ_1_ᾰ,
+        have f : false,
+          rw c at ᾰ_1_ᾰ_left,
+          injection ᾰ_1_ᾰ_left,
+        cases f,
+        cases ᾰ_1_ᾰ,
+        cases (and.elim_left ᾰ_1_ᾰ_right),
+      },
+      solve_by_elim,
+    },
+    {
+      cases ᾰ,
+      cases and.elim_left ᾰ_right,
+    }
+  },
+  {
+    assume h,
+    let z : fin 2,
+      exact 0,
+    let o : fin 2,
+      exact 1,
+    existsi z, existsi o,
+    constructor,
+    solve_by_elim,
+    constructor,
+    dsimp [single_ε_nfa],
+    rw h,
+    fconstructor,
+    exact o,
+    finish,
+    constructor,
+    solve_by_elim,
+  }
+end
+
+def union_ε_nfa {Sigma : Type*} (A : ε_nfa Sigma) (B : ε_nfa Sigma) : ε_nfa Sigma :=
+  {
+    Q := A.Q ⊕ B.Q,
+    finQ := @sum.fintype A.Q B.Q A.finQ B.finQ,
+    decQ := @sum.decidable_eq A.Q A.decQ B.Q B.decQ,
+    inits := λ q, sum.cases_on q A.inits B.inits,
+    decI := begin 
+      assume a,
+      letI dr := A.decI, letI ds := B.decI,
+      cases a;
+      tauto,
+    end,
+    final := λ q, sum.cases_on q A.final B.final,
+    decF := begin
+      assume a,
+      letI dr := A.decF, letI ds := B.decF,
+      cases a;
+      tauto,
+    end,
+    δ := λ a, sum.cases_on a
+      (λ a x b, sum.cases_on b (λ b, A.δ a x b) (λ _, false))
+      (λ a x b, sum.cases_on b (λ _, false) (λ b, B.δ a x b)),
+    decD := begin
+      assume a,
+      simp at *,
+      dsimp [sigma.uncurry],
+      cases a with ax b,
+      cases ax with a x,
+      cases a, 
+      {
+        cases b,
+        simp at *,
+        exact A.decD ⟨⟨a, x⟩, b⟩,
+        simp at *,
+        exact is_false id,
+      },
+      {
+        cases b,
+        simp at *,
+        exact is_false id,
+        simp at *,
+        exact B.decD ⟨⟨a, x⟩, b⟩,
+      }
+    end,
+  }
+
+lemma uniform_union : ∀ A B : ε_nfa Sigma, ∀ w : word Sigma, ∀ q0 q1 : A.Q ⊕ B.Q, 
+  ε_nfa_δ_star (union_ε_nfa A B) q0 w q1 → (sum.is_left q0 = sum.is_left q1) :=
+begin
+  assume A B w q0 q1,
+  assume h,
+  induction h,
+  refl,
+  rw← h_ih,
+  cases h_q0 with aq0 bq0,
+  cases h_q1 with aq1 bq1;
+  simp,
+  cases h_ᾰ,
+  cases h_q1 with aq1 bq1,
+  simp,
+  cases h_ᾰ,
+  simp,
+  rw← h_ih,
+  cases h_q0 with aq0 bq0,
+  cases h_q1 with aq1 bq1;
+  simp,
+  cases h_ᾰ,
+  cases h_q1 with aq1 bq1,
+  simp,
+  cases h_ᾰ,
+  simp,
+end
+
+lemma left_union : ∀ A B : ε_nfa Sigma, ∀ w : word Sigma, ∀ q0 q1 : A.Q,
+   ε_nfa_δ_star (union_ε_nfa A B) (sum.inl q0) w (sum.inl q1) ↔ ε_nfa_δ_star A q0 w q1 :=
+begin
+  assume A B w q0 q1,
+  constructor,
+  assume h,
+  dsimp [union_ε_nfa] at h,
+  sorry, 
+  sorry,
+end
+
+lemma right_union : ∀ A B : ε_nfa Sigma, ∀ w : word Sigma, ∀ q0 q1 : B.Q,
+   ε_nfa_δ_star (union_ε_nfa A B) (sum.inr q0) w (sum.inr q1) ↔ ε_nfa_δ_star B q0 w q1 :=
+begin
+  assume A B w q0 q1,
+  constructor,
+  assume h,
+  dsimp [union_ε_nfa] at h,
+  sorry, 
+  sorry,
+end
+
+lemma union_ε_nfa_lang : ∀ A B : ε_nfa Sigma, ∀ w : word Sigma, ε_nfa_lang (union_ε_nfa A B) w ↔ union_lang (ε_nfa_lang A) (ε_nfa_lang B) w :=
+begin
+  assume A B w,
+  constructor,
+  {
+    dsimp [ε_nfa_lang, union_lang],
+    assume h,
+    cases h with q0 h, cases h with q1 h,
+    cases q0,
+    {
+      left,
+      cases q1,
+      existsi q0, existsi q1,
+      constructor,
+      exact (and.elim_left h),
+      constructor,
+      have g : ε_nfa_δ_star (union_ε_nfa A B) (sum.inl q0) w (sum.inl q1),
+        exact (and.elim_left (and.elim_right h)),
+      exact (left_union A B w q0 q1).mp g,
+      exact (and.elim_right (and.elim_right h)),
+      have f : false,
+        have g : ε_nfa_δ_star (union_ε_nfa A B) (sum.inl q0) w (sum.inr q1),
+          exact (and.elim_left (and.elim_right h)),
+        have t := uniform_union A B w (sum.inl q0) (sum.inr q1) g,
+          simp at t,
+        exact t,
+        cases f,
+    },
+    {
+      right,
+      cases q1,
+      have g : ε_nfa_δ_star (union_ε_nfa A B) (sum.inr q0) w (sum.inl q1),
+        exact (and.elim_left (and.elim_right h)),
+      have t:= uniform_union A B w (sum.inr q0) (sum.inl q1) g,
+        simp at t,
+      cases t,
+      existsi q0, existsi q1,
+      constructor,
+      exact (and.elim_left h),
+      constructor,
+      exact (right_union A B w q0 q1).mp (and.elim_left (and.elim_right h)),
+      exact (and.elim_right (and.elim_right h)),
+    }   
+  },
+  {
+    dsimp [union_lang, ε_nfa_lang],
+    assume h,
+    cases h,
+    {
+      cases h with q0 h, cases h with q1 h,
+      existsi (sum.inl q0), existsi (sum.inl q1),
+      constructor,
+      exact (and.elim_left h),
+      constructor,
+      exact (left_union A B w q0 q1).mpr (and.elim_left (and.elim_right h)),
+      exact (and.elim_right (and.elim_right h)),
+    },
+    {
+      cases h with q0 h, cases h with q1 h,
+      existsi (sum.inr q0), existsi (sum.inr q1),
+      constructor,
+      exact (and.elim_left h),
+      constructor,
+      exact (right_union A B w q0 q1).mpr (and.elim_left (and.elim_right h)),
+      exact (and.elim_right (and.elim_right h)),
+    }
+  }
+end
+
+def star_ε_nfa {Sigma : Type*} [decidable_eq Sigma] (A : ε_nfa Sigma) : ε_nfa Sigma :=
+  {
+    Q := A.Q,
+    finQ := A.finQ,
+    decQ := A.decQ,
+    inits := A.inits,
+    decI := A.decI,
+    final := λ q, A.final q ∨ A.inits q,
+    decF := begin 
+      letI dI := A.decI,
+      letI dF := A.decF,
+      apply_instance,
+    end,
+    δ := λ a x b, A.δ a x b ∨ (A.final a ∧ A.inits b ∧ x = none),
+    decD := begin
+      assume x,
+      dsimp [sigma.uncurry],
+      cases A.decD ⟨⟨x.fst.fst, x.fst.snd⟩, x.snd⟩ with n y,
+      cases A.decF x.fst.fst with nn yy,
+      letI not : ¬(A.δ x.fst.fst x.fst.snd x.snd ∨ A.final x.fst.fst ∧ A.inits x.snd ∧ x.fst.snd = none),
+        assume h,
+        cases h with l r,
+        dsimp [sigma.uncurry] at n,
+        exact n l,
+        apply nn,
+        exact and.elim_left r,
+      exact is_false not,
+      cases A.decI x.snd with nnn yyy,
+      letI not : ¬(A.δ x.fst.fst x.fst.snd x.snd ∨ A.final x.fst.fst ∧ A.inits x.snd ∧ x.fst.snd = none),
+        assume h,
+        cases h with l r,
+        dsimp [sigma.uncurry] at n,
+        exact n l,
+        apply nnn,
+        exact and.elim_left (and.elim_right r),
+      exact is_false not,
+      letI test: decidable_pred (λ x : option Sigma, x = none),
+        apply_instance,
+      cases test x.fst.snd,
+      letI not : ¬(A.δ x.fst.fst x.fst.snd x.snd ∨ A.final x.fst.fst ∧ A.inits x.snd ∧ x.fst.snd = none),
+        assume h,
+        cases h with l r,
+        dsimp [sigma.uncurry] at n,
+        exact n l,
+        apply h,
+        exact and.elim_right (and.elim_right r),
+      exact is_false not,
+      letI yes : A.δ x.fst.fst x.fst.snd x.snd ∨ A.final x.fst.fst ∧ A.inits x.snd ∧ x.fst.snd = none,
+        right,
+        exact ⟨yy, ⟨yyy, h⟩⟩,
+      exact is_true yes,
+      letI yes : A.δ x.fst.fst x.fst.snd x.snd ∨ A.final x.fst.fst ∧ A.inits x.snd ∧ x.fst.snd = none,
+        left,
+        dsimp [sigma.uncurry] at y,
+        exact y,
+      exact is_true yes,
+    end
+  }
+
+def append_ε_nfa {Sigma : Type*} [decidable_eq Sigma] (A : ε_nfa Sigma) (B : ε_nfa Sigma) : ε_nfa Sigma :=
+  {
+    Q := A.Q ⊕ B.Q,
+    finQ := @sum.fintype A.Q B.Q A.finQ B.finQ,
+    decQ := @sum.decidable_eq A.Q A.decQ B.Q B.decQ,
+    inits := λ q, sum.cases_on q A.inits (λ _, false),
+    decI := begin
+      assume a,
+      cases a;
+      simp at *,
+      exact A.decI a,
+      exact is_false id,
+    end,
+    final := λ q, sum.cases_on q (λ _, false) B.final,
+    decF := begin
+      assume a,
+      cases a;
+      simp at *,
+      exact is_false id,
+      exact B.decF a,
+    end,
+    δ := λ a, sum.cases_on a 
+      (λ a x b, sum.cases_on b (λ b, A.δ a x b) (λ b, A.final a ∧ B.inits b ∧ x = none))
+      (λ a x b, sum.cases_on b (λ _, false) (λ b, B.δ a x b)),
+    decD := begin
+      assume a,
+      cases a with ax b, cases ax with a x,
+      cases a; cases b; dsimp [sigma.uncurry],
+      exact A.decD ⟨⟨a, x⟩, b⟩,
+      {
+        letI dF := A.decF,
+        letI dI := B.decI,
+        letI deq := @sum.decidable_eq A.Q A.decQ B.Q B.decQ,
+        apply_instance,
+      },
+      exact is_false id,
+      exact B.decD ⟨⟨a, x⟩, b⟩,
+    end,
+  }
 
 def re2ε_nfa : RE Sigma → ε_nfa Sigma
 | empty := empty_ε_nfa
@@ -53,124 +472,33 @@ begin
   {
     -- empty
     assume w,
+    dsimp [re_lang],
     dsimp [re2ε_nfa],
-    dsimp [re_lang, ε_nfa_lang],
-    constructor,
-    assume f, cases f,
-    assume h,
-    cases h with q0 h1,
-    cases h1 with q1 h2,
-    cases h2 with h3 h4,
-    cases h4 with h5 h6,
-    cases h6,
+    exact iff.symm (empty_ε_nfa_lang w),
   },
   {
     -- lit r
     assume w,
+    dsimp [re_lang],
     dsimp [re2ε_nfa],
-    dsimp [re_lang, ε_nfa_lang],
-    have zlto : ∃ z o : fin 2, z < o ∧ z.val = 0 ∧ o.val = 1,
-      exact dec_trivial,
-    constructor,
-    assume h,
-    cases zlto with z h,
-    cases h with o p,
-    existsi z,
-    existsi o,
-    constructor,
-    cases z,
-    cases o,
-    simp at p,
-    exact and.elim_left (and.elim_right p),
-    constructor,
-    dsimp [single_ε_nfa],
-    fconstructor,
-    exact z,
-    finish,
-    rw h,
-    fconstructor,
-    exact o,
-    finish,
-    fconstructor,
-    exact and.elim_right (and.elim_right p),
-    assume h,
-    cases h with q0 h2,
-    cases h2 with q1 h3,
-    cases h3 with h4 h5,
-    cases h5 with h6 h7,    
-    cases h6,
-    have different_inits_final : ∀ q : (single_ε_nfa r).Q,
-      (single_ε_nfa r).inits q ∧ (single_ε_nfa r).final q → false,
-      {
-        assume q,
-        assume h,
-        cases h with i f,
-        have q_zero : q.val = 0,
-          exact i,
-        have q_one : q.val = 1,
-          exact f,
-        finish,
-      },
-    cases (different_inits_final q0 (and.intro h4 h7)),
-    sorry,
-    cases h6_ᾰ,
-    finish,
-    sorry,
+    exact iff.symm (single_ε_nfa_lang r w),
   },
   {
     -- union
     assume w,
-    constructor,
-    dsimp [re_lang, ε_nfa_lang];
-    {
-      assume h,
-      cases h,
+    dsimp [re_lang],
+    dsimp [re2ε_nfa],
+    let g := (iff.symm (union_ε_nfa_lang (re2ε_nfa r_ᾰ) (re2ε_nfa r_ᾰ_1) w)),
+    let h : union_lang (re_lang r_ᾰ) (re_lang r_ᾰ_1) w ↔ union_lang (ε_nfa_lang (re2ε_nfa r_ᾰ)) (ε_nfa_lang (re2ε_nfa r_ᾰ_1)) w,
       sorry,
-      sorry,
-    },
-    {
-      dsimp [re_lang, ε_nfa_lang],
-      assume h,
-      sorry,
-    },
+    exact iff.trans h g,
   },
   {
     -- epsilon
     assume w,
+    dsimp [re_lang],
     dsimp [re2ε_nfa],
-    dsimp [re_lang, ε_nfa_lang],
-    constructor,
-    assume h,
-    rw h,
-    have z : fin 1,
-      exact 0,
-    existsi z,
-    existsi z,
-    constructor,
-    constructor,
-    constructor,
-    constructor,
-    constructor,
-    assume h,
-    cases h with q0 h1,
-    cases h1 with q1 h2,
-    cases h2 with h3 h4,
-    cases h4 with h5 h6,
-    let h7 : ∀ q : epsilon_ε_nfa.Q, q = q1,
-    {
-      assume q,
-      cases q,
-      cases q1,
-      simp at q_property,
-      simp at q1_property,
-      simp,
-      rw q_property,
-      rw q1_property,
-    },
-    cases h5,
-    refl,
-    solve_by_elim,
-    solve_by_elim,
+    exact iff.symm (epsilon_ε_nfa_lang w),
   },
   {
     -- star
