8eb678b578f65b0392c6b63aa7fd9cadda78b424
filikec <blabladix@gmail.com>
2023-07-22 00:35:50 +0200

    Added comments to ToNFA

--- lean4/Automaton/DFA/ToNFA.lean
+++ lean4/Automaton/DFA/ToNFA.lean
@@ -1,6 +1,13 @@
 import Automaton.DFA.Basic
 import Automaton.NFA.Basic
 
+/-!
+  This file contains conversion from DFA to NFA `dfa_to_nfa`
+  Proves several things about such converted automata such as equality of δ_star function
+  Contains a definition of what equality between NFA and DFA means
+  Then asserts that the converted DFA fulfills this property
+-/
+
 open DFA NFA
 
 namespace ToNFA
@@ -30,7 +37,7 @@ theorem dfa_to_nfa_eq_δ_star  (w : word σ) : {DFA.δ_star td w} = NFA.δ_star 
   | nil => dsimp [DFA.δ_star,NFA.δ_star]; rw [←dfa_to_nfa_eq_init]
   | cons a as h => dsimp [DFA.δ_star,NFA.δ_star]; rw [←h]; simp [δ_step,dfa_to_nfa,dfa_δ_to_nfa_δ]
 
--- converted dfa is equal to the original dfa
+-- converted dfa accepts the same language as the original dfa
 theorem dfa_to_nfa_eq : dfa_nfa_eq td (dfa_to_nfa td) := by
   dsimp [dfa_nfa_eq]
   intro w
