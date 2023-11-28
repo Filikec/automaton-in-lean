36081b72b3116307ee056c4e5e14a8f7e0e295e1
filikec <blabladix@gmail.com>
2023-11-28 18:08:14 +0000

    Updated examples

--- lean4/Automaton/DFA/Examples.lean
+++ lean4/Automaton/DFA/Examples.lean
@@ -9,15 +9,18 @@ import Mathlib.Data.Fin.Basic
 
 open Nat DFA
 
+-- need to be able to synth FinEnum to print the DFA
+def Q : Finset (Fin 2) := {0,1}
+def σ : Finset (Fin 2) := {0,1}
 
-def δ₁ : { x : Fin 2 // x ∈ ({0 , 1} : Finset (Fin 2)) } → { x : Fin 2 // x ∈ ({0 , 1} : Finset (Fin 2)) } → { x : Fin 2 // x ∈ ({0 , 1} : Finset (Fin 2))}
-  | ⟨0,_⟩ , ⟨1,_⟩ => ⟨1, by simp⟩
-  | ⟨0,_⟩ , ⟨0,_⟩ => ⟨0, by simp⟩
-  | ⟨1,_⟩ , ⟨1,_⟩ => ⟨1, by simp⟩
-  | ⟨1,_⟩ , ⟨0,_⟩ => ⟨0, by simp⟩
+def δ₁ : Q → σ → Q
+  | ⟨0,_⟩ , ⟨0,_⟩ => ⟨1, by simp⟩
+  | ⟨0,_⟩ , ⟨1,_⟩ => ⟨0, by simp⟩
+  | ⟨1,_⟩ , ⟨0,_⟩ => ⟨1, by simp⟩
+  | ⟨1,_⟩ , ⟨1,_⟩ => ⟨0, by simp⟩
 
 -- accepts all words that end with '1'
-def last_is_one : DFA (Fin 2) (Fin 2) := {qs := {0, 1}, σs := {0,1}, init := ⟨0,by simp⟩ , fs := {⟨1 , by simp⟩} , δ := δ₁}
+def last_is_one : DFA (Fin 2) (Fin 2) := {qs := Q, σs := σ , init := ⟨0,by simp⟩ , fs := {⟨1 , by simp⟩} , δ := δ₁}
 
 def w₁ : word (Fin 2) := [ 1, 0 ]
 def w₂ : word (Fin 2) := [ 0, 1 ]
@@ -25,8 +28,11 @@ def w₃ : word (Fin 2) := [ ]
 
 #eval last_is_one
 
-#eval distinct_table_filling last_is_one ⟨0,by simp⟩ ⟨1,by simp⟩
-#eval distinct_table_filling last_is_one ⟨0,by simp⟩ ⟨0,by simp⟩
+def q₁ : last_is_one.qs := ⟨0,by simp⟩
+def q₂ : last_is_one.qs := ⟨1,by simp⟩
 
-#eval distinct last_is_one ⟨0,by simp⟩ ⟨1,by simp⟩
-#eval distinct last_is_one ⟨0,by simp⟩ ⟨0,by simp⟩
+#eval distinct_table_filling q₁ q₂
+#eval distinct_table_filling q₁ q₁
+
+#eval distinct q₁ q₂
+#eval distinct q₁ q₁
